6dcad37d4d0321ec0d47b977579f044af006930b
kingho <king@real.co>
2019-04-29 16:48:34 +0800

    Initial Commit

---
+++ RTC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RTC
+//
+//  Created by king on 29/4/2019.
+//  Copyright © 2019 Real. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
