c96be5e6b0b466ac4e502ab1670ec2cc8ce2c2a1
kingho <king@real.co>
2019-06-26 18:22:13 +0800

    Add load chatroom

---
+++ RTC/Chat/Model/Chatroom.swift
@@ -0,0 +1,54 @@
+//
+//  Chatroom.swift
+//  RTC
+//
+//  Created by king on 26/6/2019.
+//  Copyright © 2019 Real. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct Chatroom {
+    var id: String
+    var users: [User]
+    var title: String?
+    var imageUrl: String?
+    var modifiedDate: Date?
+    
+    init(document: QueryDocumentSnapshot) {
+        let data = document.data()
+        
+        self.id = document.documentID
+        
+        self.users = [User]()
+        if let roomUsers = data[ChatManager.Constants.keyUsers] as? [[String : Any]] {
+            for roomUser in roomUsers {
+                guard let roomUserId = roomUser[ChatManager.Constants.keyUserId] as? String,
+                    let roomUserRole = roomUser[ChatManager.Constants.keyRole] as? Int else {
+                        continue
+                }
+                
+                self.users.append(User(userId: roomUserId, role: ChatManager.Role(rawValue: roomUserRole) ?? ChatManager.Role.member))
+            }
+        }
+        
+        self.title = data[ChatManager.Constants.keyTitle] as? String
+        self.imageUrl = data[ChatManager.Constants.keyImageUrl] as? String
+        if let modifiedTime = data[ChatManager.Constants.keyModifiedDate] as? Timestamp {
+            self.modifiedDate = Date(timeIntervalSince1970: TimeInterval(modifiedTime.seconds))
+        }
+    }
+}
+
+extension Chatroom: Comparable {
+    
+    static func == (lhs: Chatroom, rhs: Chatroom) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    static func < (lhs: Chatroom, rhs: Chatroom) -> Bool {
+        return lhs.id < rhs.id
+    }
+    
+}
---
+++ RTC/ChatroomViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ChatroomViewController.swift
+//  RTC
+//
+//  Created by king on 29/4/2019.
+//  Copyright © 2019 Real. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ChatroomViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView?
+    
+    private let chatroomCellIdentifier = "chatroomCell"
+    
+    private var app = UIApplication.shared.delegate as? AppDelegate
+    
+    private var chatroomListener: ListenerRegistration?
+    
+    private var chatrooms = [Chatroom]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addChatroomListener()
+    }
+    
+    private func addChatroomListener() {
+        let db = Firestore.firestore()
+        chatroomListener = db.collection(ChatManager.Constants.keyChatrooms)
+            .order(by: ChatManager.Constants.keyModifiedDate, descending: true)
+            .addSnapshotListener { [weak self] (documentSnapshot, error) in
+                documentSnapshot?.documentChanges.forEach({ [weak self] (diff) in
+                    let document = diff.document
+                    
+                    switch diff.type {
+                    case .added:
+                        let chatroom = Chatroom(document: document)
+                        self?.chatrooms.append(chatroom)
+                        break
+                    case .removed:
+                        
+                        break
+                    default:
+                        break
+                    }
+                })
+                
+                self?.tableView?.reloadData()
+        }
+    }
+    
+    @IBAction func createChatRoom() {
+        let userAdmin = ChatManager.User(userId: "1", role: ChatManager.Role.admin)
+        let userMember = ChatManager.User(userId: "2", role: ChatManager.Role.member)
+        app?.chatManager?.createChatroom(users: [userAdmin, userMember], title: "test room", imageUrl: "image.jpg", completionHandler: { (roomId) in
+            print(roomId ?? "createChatRoom error")
+        })
+    }
+    
+    @IBAction func createChatRoomAndSendMessage() {
+        let userAdmin = ChatManager.User(userId: "1", role: ChatManager.Role.admin)
+        let userMember = ChatManager.User(userId: "2", role: ChatManager.Role.member)
+        app?.chatManager?.createChatroom(users: [userAdmin, userMember], title: "test room", imageUrl: "image.jpg", completionHandler: { [weak self] (roomId) in
+            print(roomId ?? "createChatRoom error")
+            
+            if let roomId = roomId {
+                self?.app?.chatManager?.createMessage(forRoomId: roomId, content: "test message content", senderId: "1", completionHandler: { (messageId) in
+                    print(messageId ?? "create message error")
+                })
+            }
+        })
+    }
+    
+    @IBAction func queryChatRooms() {
+        app?.chatManager?.queryAllChatrooms(completionHandler: { (rooms) in
+            guard let rooms = rooms else {
+                return
+            }
+            
+            for document in rooms {
+                print("\(document.documentID) => \(document.data())")
+            }
+        })
+    }
+    
+}
+
+extension ChatroomViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatrooms.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: chatroomCellIdentifier, for: indexPath)
+        let cell = UITableViewCell(style: .default, reuseIdentifier: chatroomCellIdentifier)
+        
+        let chatroom = chatrooms[indexPath.row]
+        cell.textLabel?.text = chatroom.id
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        if let date = chatroom.modifiedDate {
+            cell.detailTextLabel?.text = formatter.string(from: date)
+        } else {
+            cell.detailTextLabel?.text = ""
+        }
+        
+        return cell
+    }
+
+}
