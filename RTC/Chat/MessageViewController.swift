0ed8c06a8ac7422b1c2597c9511155efa650ddff
kingho <king@real.co>
2019-07-03 13:23:46 +0800

    Add listener to last message and pagination to previous message.

--- RTC/Chat/MessageViewController.swift
+++ RTC/Chat/MessageViewController.swift
@@ -17,8 +17,13 @@ class MessageViewController: UIViewController {
     
     private var app = UIApplication.shared.delegate as? AppDelegate
     private var messageListener: ListenerRegistration?
+    private var previousMessageListener: ListenerRegistration?
     private let db = Firestore.firestore()
     private(set) var messages = [Message]()
+    
+    private var currentSnapshot: QuerySnapshot?
+    
+    private var currentLimit = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +39,8 @@ class MessageViewController: UIViewController {
         messageListener = db.collection(ChatManager.Constants.keyChatrooms)
             .document(chatroomId)
             .collection(ChatManager.Constants.keyMessages)
-            .order(by: ChatManager.Constants.keyModifiedDate, descending: false)
+            .order(by: ChatManager.Constants.keyModifiedDate, descending: true)
+            .limit(to: currentLimit)
             .addSnapshotListener({ [weak self] (documentSnapshot, error) in
                 guard let `self` = self else {
                     return
@@ -44,6 +50,10 @@ class MessageViewController: UIViewController {
                     self.messages.removeAll()
                     self.tableView?.reloadData()
                     return
+                }
+                
+                if self.currentSnapshot == nil {
+                    self.currentSnapshot = documentSnapshot
                 }
                 
                 documentSnapshot?.documentChanges.forEach({ [weak self] (diff) in
@@ -57,13 +67,15 @@ class MessageViewController: UIViewController {
                     switch diff.type {
                     case .added:
                         self.messages.append(message)
+                        self.messages.sort()
+//                        self.currentLimit += 1
                         break
                     case .removed:
                         guard let index = self.messages.firstIndex(of: message) else {
                             return
                         }
                         
-                        self.messages.remove(at: index)
+//                        self.messages.remove(at: index)
                         break
                     case .modified:
                         guard let index = self.messages.firstIndex(of: message) else {
@@ -92,6 +104,68 @@ class MessageViewController: UIViewController {
             print("messageId=\(String(describing: messageId))")
         }
     }
+    
+    @IBAction func previousPage() {
+        guard let currentSnapshot = currentSnapshot, let last = currentSnapshot.documents.last, let chatroomId = chatroomId else {
+            return
+        }
+        
+        previousMessageListener = db.collection(ChatManager.Constants.keyChatrooms)
+            .document(chatroomId)
+            .collection(ChatManager.Constants.keyMessages)
+            .order(by: ChatManager.Constants.keyModifiedDate, descending: true)
+            .limit(to: 2)
+            .start(afterDocument: last)
+            .addSnapshotListener({ [weak self] (documentSnapshot, error) in
+                guard let `self` = self else {
+                    return
+                }
+                
+                guard error == nil else {
+                    self.messages.removeAll()
+                    self.tableView?.reloadData()
+                    return
+                }
+                
+                self.currentSnapshot = documentSnapshot
+                
+                documentSnapshot?.documentChanges.forEach({ [weak self] (diff) in
+                    guard let `self` = self else {
+                        return
+                    }
+                    
+                    let document = diff.document
+                    let message = Message(document: document)
+                    
+                    switch diff.type {
+                    case .added:
+                        self.messages.append(message)
+                        self.messages.sort()
+                        
+                        break
+                    case .removed:
+                        guard let index = self.messages.firstIndex(of: message) else {
+                            return
+                        }
+                        
+                        //                        self.messages.remove(at: index)
+                        break
+                    case .modified:
+                        guard let index = self.messages.firstIndex(of: message) else {
+                            return
+                        }
+                        
+                        self.messages[index] = message
+                        break
+                    default:
+                        break
+                    }
+                })
+                
+                self.tableView?.reloadData()
+            })
+        
+    }
 }
 
 extension MessageViewController: UITableViewDataSource {
@@ -111,6 +185,14 @@ extension MessageViewController: UITableViewDelegate {
         let message = messages[indexPath.row]
         
         cell.textLabel?.text = message.content
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        if let date = message.modifiedDate {
+            cell.detailTextLabel?.text = formatter.string(from: date)
+        } else {
+            cell.detailTextLabel?.text = ""
+        }
         
         return cell
     }
